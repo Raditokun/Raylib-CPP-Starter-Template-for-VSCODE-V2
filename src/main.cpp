82087ed77fe0feee85f88997e8a560d2d6d59719
Raditokun <137684357+Raditokun@users.noreply.github.com>
2026-02-04 11:57:32 +0700

    pingpoing

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,69 @@
 #include <raylib.h>
-#include "ball.h"
+#include <iostream>
 
-int main() 
-{
-    const Color darkGreen = {20, 160, 133, 255};
+using namespace std;
+
+class Ball{
+    public:
+    float x, y;
+    int speedx,speedy;
+    int radius;
+
+    void Draw(){
+        //Cirlce
+    DrawCircle(x,y,radius, WHITE);
+
+    };
+
+    void Update(){
+        x += speedx;
+        y += speedy;
+    };
+};
+
+Ball ball;
+int main(){
+
+    Color green= {20,160,133,255};
+
+    cout << "starting the gem";
+    const int res_width = 1280;
+    const int res_height = 800; 
+
+   InitWindow(res_width, res_height, "nyoba game di raylib");
+   SetTargetFPS(60);
+
+   ball.radius = 20;
+   ball.x = res_width/2;
+   ball.y = res_height/2;
+   ball.speedx = 7;
+   ball.speedy = 7;
+
+   while(WindowShouldClose() == false){
+    BeginDrawing();
+
     
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 600;
     
-    Ball ball;
-    
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(60);
-    
-    while (!WindowShouldClose())
-    {
-        ball.Update();
-        
-        BeginDrawing();
-            ClearBackground(darkGreen);
-            ball.Draw();
-        EndDrawing();
-    }
-    
-    CloseWindow();
+    ball.Update();
+    //rectanggle
+    DrawRectangle(0,res_height/2-60,25, 120, WHITE);
+    DrawRectangle(res_width - 20, res_height/2-60,25,120,WHITE);
+
+
+    //board
+    DrawLine(res_width/2,0,res_width/2,res_height,WHITE);
+    ball.Draw();
+
+    EndDrawing();
+
+   }
+
+  
+   
+
+
+
+
+   CloseWindow();
+    return 0;
 }
